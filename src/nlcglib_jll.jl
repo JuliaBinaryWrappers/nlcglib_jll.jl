e6121cbc59aa3144ef9ccbd9d89f719696aa1b12
jlbuild <juliabuildbot@gmail.com>
2026-03-23 14:26:46 +0000

    nlcglib_jll build 1.1.1+0

--- src/nlcglib_jll.jl
+++ src/nlcglib_jll.jl
@@ -8,5 +8,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("nlcglib")
-JLLWrappers.@generate_main_file("nlcglib", UUID("782cc49c-e073-530d-b5fe-579a70b042cc"))
+JLLWrappers.@generate_main_file("nlcglib", Base.UUID("782cc49c-e073-530d-b5fe-579a70b042cc"))
 end  # module nlcglib_jll
